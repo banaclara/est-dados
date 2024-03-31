1ff60c45fe040176f5eb8ed616e6074814b978b6
Ana <anaclaraqb@gmail.com>
2024-03-31 15:54:30 -0300

    edit

--- lista04/07.py
+++ lista04/07.py
@@ -4,17 +4,23 @@ vetor_int = [5, 5, 5, 10, 5, 10, 5, 76, 90, 90, 90, 10, 15, 5, 25, 37, 8, 82, 82
 
 def terceiro_maior(vetor):
     maior = vetor[0]
-    segundo_maior = vetor[0]
-    terceiro_maior = vetor[0]
     for n in vetor:
         if n > maior:
             maior = n
+    while maior in vetor:
+        vetor.remove(maior)
+
+    maior2 = vetor[0]
     for n in vetor:
-        if n > segundo_maior and n < maior:
-            segundo_maior = n
+        if n > maior2:
+            maior2 = n
+    while maior2 in vetor:
+        vetor.remove(maior2)
+
+    maior3 = vetor[0]
     for n in vetor:
-        if n > terceiro_maior and n < segundo_maior:
-            terceiro_maior = n
-    return terceiro_maior
+        if n > maior3:
+            maior3 = n
+    return maior3
 
 print(terceiro_maior(vetor_int))
